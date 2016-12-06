af2fbb3931e34e37abf8183da5eec4b44147a7c0
zaouali <hamza.zaouali@eurecom.fr>
2016-12-06 15:38:43 +0100

    adding transition

--- app/src/main/java/com/mobserve/weew/weew/entry.java
+++ app/src/main/java/com/mobserve/weew/weew/entry.java
@@ -1,16 +1,25 @@
 package com.mobserve.weew.weew;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class entry extends AppCompatActivity {
+
+public class entry extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
-        startActivity(new Intent(entry.this, LoginActivity.class));
-    }
 
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                entry.this.startActivity(new Intent(entry.this, LoginActivity.class));
+                entry.this.finish();
+            }
+        }, 2000);
+    }
 }
