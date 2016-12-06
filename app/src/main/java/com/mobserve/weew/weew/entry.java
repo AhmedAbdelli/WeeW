7627d790d9fd12603a5a8f7f4ca73bc812fc40fd
AhmedAbdelli <ahmedabdelli.sup@gmail.com>
2016-12-06 11:58:04 +0100

    Initial commit

---
+++ app/src/main/java/com/mobserve/weew/weew/entry.java
@@ -0,0 +1,13 @@
+package com.mobserve.weew.weew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class entry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+    }
+}
