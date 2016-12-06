5415f8eecc9e585616bde552e50c8c6795c4e461
zaouali <hamza.zaouali@eurecom.fr>
2016-12-06 15:14:49 +0100

    fix login activity

--- app/src/main/java/com/mobserve/weew/weew/entry.java
+++ app/src/main/java/com/mobserve/weew/weew/entry.java
@@ -1,5 +1,6 @@
 package com.mobserve.weew.weew;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class entry extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+        startActivity(new Intent(entry.this, LoginActivity.class));
     }
+
 }
